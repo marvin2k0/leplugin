70a72845acde19520d2beff3c3693c26a4bddeb2
Marvin Leiers <marvin@leiers.org>
2023-11-24 15:59:38 +0100

    Added FileConfiguration to Messages

--- leplugin-api/src/main/java/org/leiers/leplugin/api/config/Messages.java
+++ leplugin-api/src/main/java/org/leiers/leplugin/api/config/Messages.java
@@ -1,9 +1,13 @@
 package org.leiers.leplugin.api.config;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 public interface Messages<T> {
     T get(String path);
 
     T get(String path, boolean usePrefix);
 
     T getPrefix();
+
+    FileConfiguration getConfig();
 }
